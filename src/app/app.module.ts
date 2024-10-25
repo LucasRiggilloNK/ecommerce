8ba30ca6e7c4de990769fa0439f41e335f127b5a
BlancoRioNicolas <nicolasuxmal@gmail.com>
2024-10-25 20:34:52 -0300

    Import reactig form from Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
+    ReactiveFormsModule,
     AppComponent,
     HeaderComponent,
     HomeComponent,
--- src/app/models/users/user.ts
+++ src/app/models/users/user.ts
@@ -1,9 +1,2 @@
 import internal from "stream";
 
-export class User {
-    name: String,
-    lastName: String,
-    dateOfBirth: dateTime,
-    email: String,
-    phoneNumber: int,
-}
