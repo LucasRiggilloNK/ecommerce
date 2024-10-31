cede65ce25f0b37f0accecb7138ff5b8da9822de
“lucasRiggillo” <lucasRiggillo>
2024-10-31 19:17:30 -0300

    Conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,15 +14,6 @@ import {
 } from '@angular/common/http';
 import { AuthInterceptor } from './services/login/auth.interceptor';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { AirConditioningCharacteristicsComponent } from './components/characteristics/air-conditioning-characteristics/air-conditioning-characteristics.component';
-import { TvCharacteristicsComponent } from './components/characteristics/tv-characteristics/tv-characteristics.component';
-import { FanCharacteristicsComponent } from './components/characteristics/fan-characteristics/fan-characteristics.component';
-import { HeapphonesCharacteristicsComponent } from './components/characteristics/heapphones-characteristics/heapphones-characteristics.component';
-import { RefrigeratorCharacteristicsComponent } from './components/characteristics/refrigerator-characteristics/refrigerator-characteristics.component';
-import { WashingCharacteristicsComponent } from './components/characteristics/washing-characteristics/washing-characteristics.component';
-import { ViewProductComponent } from './components/products/view/view-product/view-product.component';
-import { ProductDetailsComponent } from './components/products/details/product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -32,25 +23,12 @@ import { ProductDetailsComponent } from './components/products/details/product-d
     RegisterComponent,
     LoginComponent,
     PrivateComponent,
-    AddProductComponent,
-    AirConditioningCharacteristicsComponent,
-    TvCharacteristicsComponent,
-    FanCharacteristicsComponent,
-    HeapphonesCharacteristicsComponent,
-    RefrigeratorCharacteristicsComponent,
-    WashingCharacteristicsComponent,
-    ViewProductComponent,
-    ProductDetailsComponent,
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideHttpClient(withFetch()),
   ],
-
   bootstrap: [AppComponent],
 })
 export class AppModule {}
