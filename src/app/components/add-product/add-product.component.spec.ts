de615ec995a0ddd140560a12fde43ff2372dfe4b
“lucasRiggillo” <lucasRiggillo>
2024-10-31 19:02:38 -0300

    Resolve conflict

--- src/app/components/add-product/add-product.component.spec.ts
+++ src/app/components/add-product/add-product.component.spec.ts
@@ -1,20 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:src/app/register/register.component.spec.ts
-import { RegisterComponent } from './register.component';
-
-describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RegisterComponent);
-========
 import { AddProductComponent } from './add-product.component';
 
 describe('AddProductsComponent', () => {
@@ -23,12 +8,10 @@ describe('AddProductsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AddProductComponent]
-    })
-    .compileComponents();
+      declarations: [AddProductComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddProductComponent);
->>>>>>>> epica-productos:src/app/components/add-product/add-product.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
