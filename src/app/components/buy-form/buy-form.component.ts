25d3cff3a9a6162753c45c40686e02ac6db13f5c
marquitos1985 <marquitos1985@hotmail.com>
2024-11-13 22:43:16 -0300

    Continuacion formulario de compra

--- src/app/components/buy-form/buy-form.component.ts
+++ src/app/components/buy-form/buy-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductInterface } from '../../interfaces/product/product-interface';
+import { CarritoService } from '../../services/cart.service';
+import { BuyService } from '../../services/buy.service';
+import { User } from '../../services/register-service/register.service';
 
 @Component({
   selector: 'app-buy-form',
   templateUrl: './buy-form.component.html',
   styleUrl: './buy-form.component.css'
 })
-export class BuyFormComponent {
+export class BuyFormComponent implements OnInit{
+   cartItems: ProductInterface[] = [];
+   user: User | null;
+
+  constructor(private buySeervice: BuyService){
+    this.user = null;
+  }
+
+  ngOnInit(): void {
+      this.cartItems = this.buySeervice.getCartItemsToBuy();
+      
+  }
+
+
 
 }
--- src/app/services/buy.service.ts
+++ src/app/services/buy.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { ProductInterface } from '../interfaces/product/product-interface';
+import { CarritoService } from './cart.service';
+import { User } from './register-service/register.service';
+import { AuthService } from './login/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BuyService {
+export class BuyService{
 
-  constructor() { }
+  private cartItems: ProductInterface[] = [];
+  private user: User | null;
+  
+
+  constructor(private cartService: CarritoService, private authService: AuthService){
+    this.user = null;
+  }
+
+
+  public getCartItemsToBuy(){
+    this.cartItems = this.cartService.getCartItems();
+    return this.cartItems;
+
+  }
+
+  public getUser(){
+    let userId = this.authService.getUserId();
+    
+  }
+
+  getSubtotal(){
+
+  }
 }
