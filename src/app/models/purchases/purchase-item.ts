fcee62ad2383399da108f73171a244db0d88b764
BlancoRioNicolas <nicolasuxmal@gmail.com>
2025-02-03 16:34:18 -0300

    Eliminados archivos innecesarios

--- src/app/models/purchases/purchase-item.ts
+++
@@ -1,8 +0,0 @@
-import { ProductInterface2 } from "../../interfaces/product/product-interface2";
-
-export interface PurchaseItem {
-    id: string;
-    description: string;
-    cantidad: Number;
-    producto: ProductInterface2;
-}
